ee2f7927a156cc80d21ad01cbc94112c19a787c0
Jamin Ghata <KingJamin@hotmail.com>
2019-10-25 17:24:32 -0500

    added react-bootstrap and a few buttons

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
+import {Button} from 'react-bootstrap';
+import {ButtonGroup} from 'react-bootstrap';
+import {Form} from 'react-bootstrap';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div class="App-header">
+            
+            <h1>Welcome to High-Lo Dice</h1>
+            
+            <p class="App">
+                <Form>
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Control as="input" rows="1" res/>
+                    </Form.Group>
+                </Form>
+            </p>
+
+            <p class="App">
+                Current Bet Amount
+            </p>
+<ButtonGroup aria-label="Basic example">
+  <Button variant="secondary">Left</Button>
+  <Button variant="secondary">Middle</Button>
+  <Button variant="secondary">Right</Button>
+</ButtonGroup>
+            <p class="App">
+                <Button variant="betHigh" size="lg" block>
+                    Bet Lo
+                </Button>
+                <Button variant="primary" size="lg" block>
+                    Bet High
+                </Button>
+
+            </p>
+
+        </div>
+
+    );
+
 }
 
 export default App;
